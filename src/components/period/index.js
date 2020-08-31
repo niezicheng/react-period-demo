ad5aee57a1ff7032f84e06458235c531418dac6a
nzc <nzc@nzcdeMacBook-Pro.local>
2020-08-31 20:05:44 +0800

    init project

---
+++ src/components/period/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Button, Space } from 'antd';
+import PeriodChild from '../periodChild';
+import OldPeriodChild from '../oldPeriodChild';
+
+class Period extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0,
+    }
+  }
+  render() {
+    const { count } = this.state;
+    return (
+      <>
+        <div>父组件count: { count }</div>
+        <Space>
+          <Button type="primary" onClick={() => {this.setState({ count: count + 1 })}}>+</Button>
+          <Button type="primary" onClick={() => { this.setState({ count: count - 1 })}}>-</Button>
+        </Space>
+        <PeriodChild count={this.state.count} />
+        {/* <OldPeriodChild count={this.state.count} /> */}
+      </>
+    );
+  }
+}
+
+export default Period;
