6c70ea40ec6cb7ce960941bc276aa96d6f2f3e54
nzc <nzc@nzcdeMacBook-Pro.local>
2020-09-02 20:20:22 +0800

    fix: move period some files

--- src/components/period/index.js
+++ src/components/period/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, Space } from 'antd';
-import PeriodChild from '../periodChild';
-import OldPeriodChild from '../oldPeriodChild';
+import PeriodChild from './periodChild';
+import OldPeriodChild from './oldPeriodChild';
 
 class Period extends React.PureComponent {
   constructor(props) {
